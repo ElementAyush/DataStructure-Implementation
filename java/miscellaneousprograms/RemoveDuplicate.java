de8fca97fa30d962b7f42d19a0fcc29f14042675
Ayush Dixit <ele.ayush@hotmail.com>
2021-06-10 11:48:27 +0530

    Added remove duplicate

---
+++ java/miscellaneousprograms/RemoveDuplicate.java
@@ -0,0 +1,22 @@
+class RemoveDuplicate{
+
+	public static void main(String[] args){
+		String str = "aayyush" ;
+		StringBuilder stb = new StringBuilder() ;
+		boolean[] uniqueArray = new boolean[26] ;
+        
+        for(int i = 0 ; i < str.length() ; i++){
+
+        	if(uniqueArray[str.charAt(i) - 'a'] == true)
+        	{
+        		continue ;
+        	}
+        	else{
+        		stb.append(str.charAt(i)) ;
+        		uniqueArray[str.charAt(i) - 'a'] = true ;
+        	}	
+        }
+        System.out.println(stb) ;
+
+	}
+}
