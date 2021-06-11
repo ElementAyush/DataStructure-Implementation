5de8dca86d83ffb2ec4429f70b8c968c2d108f3d
Ayush Dixit <ele.ayush@hotmail.com>
2021-06-11 15:50:27 +0530

    Update ExecutorExamples.java

--- java/ThreadCode/ExecutorExamples.java
+++ java/ThreadCode/ExecutorExamples.java
@@ -1,4 +1,16 @@
+import java.util.concurrent.Executors ;
+import java.util.concurrent.ExecutorService ;
+
 class ExecutorExamples{
 
-	
+	public static void main(String[] args) throws InterruptedException{
+
+		ExecutorService executor = Executors.newSingleThreadExecutor() ; 
+		executor.execute(()-> {
+			System.out.print("Hello ") ;
+			throw new RuntimeException("Planned exception after execute()");
+	});
+		Thread.sleep(1000);
+		executor.execute(()-> System.out.println(" World!!!"));
+	}
 }
