40808f8e4cf9793fc8054c89ab7db0df3a3d1e21
Ayush Dixit <ele.ayush@hotmail.com>
2021-06-13 22:00:36 +0530

    Update Tree.java

--- Tree/Tree.java
+++ Tree/Tree.java
@@ -1,46 +1,73 @@
-class Tree{
+class Node{
 
-   static class Node{
       int data ;
       Node left, right ;
-   	Node(int data){
-   		this.data = data ;
-         left = null ;
-         right = null ;
-   	}
+      Node(){
+         left=right= null ;
+      }
+      Node(int data){
+         this.data = data ;
+         left = right = null ;
+      }
+}
+
+class Tree{
+
+/** Method 1 to Implement a tree */
+   public void addNode(Node root , int value){
+
+       if(root.data >= value && root.left == null){
+          root.left = new Node(value);
+         }
+       else if(root.data >= value && root.left != null){
+         addNode(root.left, value) ;
+        }
+       else if(root.data < value && root.right == null){
+         root.right = new Node(value) ;
+        }
+       else if(root.data < value && root.left != null){
+         addNode(root.right, value) ;
+        }
    }
 
+/** Method 2 to Implement a tree with less if else  */
+   public Node addNodeMethod2(Node root , int value){
+      if(root == null)
+         return new Node(value) ;
 
-   public void addNode(Node root , Node newNode){
-   
-      if(root.data >= newNode.data){
-         addNode(root.left , newNode) ;
-         root.left = newNode ;
+      if(root.data >= value){
+        root.left = addNodeMethod2(root.left, value) ;
+      }else{
+         root.right = addNodeMethod2(root.right, value) ;
       }
-      else{
-         addNode(root.right , newNode) ;
-         root.right = newNode ;
-      }
+      return root ;
    }
 
-   // public void preNode(Node root){
+   public void preNode(Node root){
 
-   //    if(root == null)
-   //       return ;
+      if(root == null)
+         return ;
 
-   //    System.out.println(root.data) ;
-   //    preNode(root.left) ;
-   //    preNode(root.right) ;
-   // }
+      System.out.println(root.data) ;
+      preNode(root.left) ;
+      preNode(root.right) ;
+   }
 
 	public static void main(String[] args){
       Tree tree = new Tree() ;
       Node root = new Node(10) ;
-      tree.addNode(root, new Node(12)) ;
-      tree.addNode(root, new Node(9)) ;
-      tree.addNode(root, new Node(11)) ;
-      // Tree tree = new Tree(5) ;
+      // tree.addNode(root, 12) ;
+      // tree.addNode(root, 9) ;
+      // tree.addNode(root, 11) ;
       // tree.addNode(root, 4);
       // tree.addNode(root, 12);
+      tree.addNodeMethod2(root, 12) ;
+      tree.addNodeMethod2(root, 9) ;
+      tree.addNodeMethod2(root, 11) ;
+      tree.addNodeMethod2(root, 4) ;
+      tree.addNodeMethod2(root, 12) ;
+
+      tree.preNode(root) ;
+      
 	}
 }
