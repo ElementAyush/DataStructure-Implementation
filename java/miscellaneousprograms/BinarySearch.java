6750eca54abcd120c552a0b2efdc46fb5191e5cc
Ayush Dixit <ele.ayush@hotmail.com>
2021-06-19 11:20:06 +0530

    Update BinarySearch.java

--- java/miscellaneousprograms/BinarySearch.java
+++ java/miscellaneousprograms/BinarySearch.java
@@ -7,17 +7,17 @@ class BinarySearch{
 		int end = arrayLength - 1;
 		int mid = (start + end) / 2 ;
 
-		while(mid < end && mid >= start){
-		
+		while(mid <= end && mid >= start){
+		    
 			if(array[mid] == element){
-				System.out.println(mid) ;
 				return true ;
 			}
-			else if(array[mid] > element){
+			if(array[mid] > element){
 				mid = (start + mid) / 2 ;
 			}
-			else{
+			if(array[mid] < element){
 				mid = (mid + end) / 2 ;
+				System.out.println(mid) ;
 			}
 		} 
 		return false ;
@@ -26,7 +26,7 @@ class BinarySearch{
 	public static void main(String[] args){
 
 		int[] array = new int[]{10,11,12,13,14,15,16,17} ;
-		System.out.println(BinarySearch.checkElementExists(array, 10)) ;
+		System.out.println(BinarySearch.checkElementExists(array, 17)) ;
 		
 	}
 }
