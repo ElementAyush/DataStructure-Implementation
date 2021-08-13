461cf3e32c22d2a4aa6454d2dfa249885f688c64
Ayush Dixit <ele.ayush@hotmail.com>
2021-08-13 10:47:59 +0530

    Create PreOrderTraversal.java

---
+++ Tree/PreOrderTraversal.java
@@ -0,0 +1,16 @@
+class PreOrderTraversal{
+  static class Node{
+    int data;
+    Node left, right ;
+    void Node(int data){
+      this.data = data ;
+      left = right = null ;
+    }
+  }
+
+
+  public static void main(String[] args){
+    Node root = new Node(1) ;
+    
+  }
+}
