e2114aa5e4481ca88fc6b7e7c252789d21f4f28d
Ayush Dixit <ele.ayush@hotmail.com>
2021-08-25 21:10:10 +0530

    Update Heap.java

--- Heap/Heap.java
+++ Heap/Heap.java
@@ -1,4 +1,17 @@
 class Heap {
+  int[] nodes ;
+  Heap(int n){
+    nodes = new int[n] ;
+}
+  public static void main(String args[]){
+  /*
+          10
+         /  \
+        6    12
+       / \  / \
+      5   7 11 15
+  */
 
-  
+  Heap heap = new Heap(7) ;
+  }
 }
