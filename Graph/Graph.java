ed5c11dbf05149b3b38fea55c12911dc2780afc2
Ayush Dixit <ele.ayush@hotmail.com>
2022-10-03 18:29:15 +0200

    Graph Implementation

--- Graph/Graph.java
+++ Graph/Graph.java
@@ -1,6 +1,18 @@
 import java.util.ArrayList;
 import java.util.List;
 
+public class Edge{
+
+    Edge leftNode, rightNode ;
+    int weight; 
+    Edge(Edge left, Edge right, int weight){
+        leftNode = left;
+        rightNode = right ;
+        this.wight = weight ;
+
+    }
+}
+
 public class Graph {
     private List<Integer>[] adj ;
     private int V ;
@@ -10,8 +22,11 @@ public class Graph {
         for(int i = 0 ; i < V ; i++)
             adj[i] = new ArrayList<>() ;
     }
+
+    public void addEdge()
     public static void main(String[] args) {
         Graph g = new Graph(5) ;
+
 
     }
 }
---
+++ Graph/Temp.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Temp{
+	
+	public static void main(String[] args){
+
+		List<Integer>[] arrayOfInteger = new ArrayList[6] ;
+
+		arrayOfInteger[0] = new ArrayList<>() ;
+
+		// for(int i = 0 ; i < arrayOfInteger.length ; i++){
+		// 	System.out.println(arrayOfInteger[i]) ;
+		// }
+
+	}
+}
